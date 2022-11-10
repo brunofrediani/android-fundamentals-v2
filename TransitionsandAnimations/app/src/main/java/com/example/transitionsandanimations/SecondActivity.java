1c802613b0c91ec0c78a043dc5ba914b1840aba5
Bruno Frediani <brunojfredi@gmail.com>
2022-11-10 18:54:38 -0300

    Android fundamentals 05.2: Cards and colors Coding Challenge

---
+++ TransitionsandAnimations/app/src/main/java/com/example/transitionsandanimations/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.transitionsandanimations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+public class SecondActivity extends BaseClass{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+        // Initialize the views.
+        ImageView redBlock = findViewById(R.id.redBlock);
+        ImageView blueBlock = findViewById(R.id.blueBlock);
+        ImageView yellowBlock = findViewById(R.id.yellowBlock);
+        ImageView androidBlock = findViewById(R.id.androidBlock);
+
+
+        // Set the methods from the base class to the appropriate ImageViews.
+        explodeTransition(this, redBlock);
+        fadeTransition(this, blueBlock);
+        rotateView(yellowBlock);
+        switchAnimation(androidBlock,blueBlock, new Intent(
+                this, MainActivity.class),this);
+    }
+}
